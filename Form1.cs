68034281d05d7f46a9b62f32e02c8b948568e117
SimpleFNAFer <tncheetah758@gmail.com>
2020-11-04 18:31:44 +0300

    no message

--- Form1.cs
+++ Form1.cs
@@ -18,45 +18,15 @@ namespace TableAnalizing
             InitializeComponent();
         }
 
+        private string keycon = "Data Source=DNSNASY\\NASYSERVER;Initial Catalog=INFORMATICS;Integrated Security=True";
+
         private string[] req = {"SELECT * FROM [INFORMATICS].[dbo].[Students]",
-        "CREATE TABLE Results (ID int, ФИО nvarchar(100), Лагерь nvarchar(100), Статус nchar(10))"};
+        "CREATE TABLE Results (ID int, ФИО nvarchar(100), Лагерь nvarchar(100), Статус nchar(10))",
+        "INSERT INTO [INFORMATICS].[dbo].[Students] (ФИО, СданныеПрофвзносы, Месяц, Сумма) VALUES (@TEXT1, @TEXT2, @TEXT3, @TEXT4);"};
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = null;
-            SqlCommand cmd = null;
-            SqlDataReader red = null;
-
-            try
-            {
-                dataGridView1.Rows.Clear();
-                dataGridView1.Columns.Clear();
-
-                con = new SqlConnection("Data Source=DNSNASY\\NASYSERVER;Initial Catalog=INFORMATICS;Integrated Security=True");
-                cmd = new SqlCommand(req[0], con);
-                con.Open();
-                red = cmd.ExecuteReader();
-
-                dataGridView1.Columns.Add("ID", "ID");
-                dataGridView1.Columns.Add("ФИО", "ФИО");
-                dataGridView1.Columns.Add("СданныеПрофвзносы", "СданныеПрофвзносы");
-                dataGridView1.Columns.Add("Месяц", "Месяц");
-                dataGridView1.Columns.Add("Сумма", "Сумма");
-                while (red.Read())
-                {
-                    string[] temp = { red[0].ToString(), red[1].ToString(), red[2].ToString(), red[3].ToString(), red[4].ToString() };
-                    
-                    dataGridView1.Rows.Add(temp);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-            finally
-            {
-                con.Close();
-            }
+            refresh();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -69,7 +39,7 @@ namespace TableAnalizing
                 dataGridView1.Rows.Clear();
                 dataGridView1.Columns.Clear();
                 
-                con = new SqlConnection("Data Source=DNSNASY\\NASYSERVER;Initial Catalog=INFORMATICS;Integrated Security=True");
+                con = new SqlConnection(keycon);
                 /*cmd = new SqlCommand(req[1], con);
                 con.Open();
                 cmd.ExecuteScalar();
@@ -212,7 +182,91 @@ namespace TableAnalizing
             }
             finally
             {
+                if ((con.State != ConnectionState.Closed) || (con != null))
+                {
+                    con.Close();
+                }
+            }
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            try
+            {
+                con = new SqlConnection("Data Source = DNSNASY\\NASYSERVER; Initial Catalog = INFORMATICS; Integrated Security = True");
+                cmd = new SqlCommand(req[2], con);
+                if((textBox1.Text != string.Empty)&& (textBox2.Text != string.Empty)&& (textBox3.Text != string.Empty)&& (textBox4.Text != string.Empty))
+                {
+                    cmd.Parameters.AddWithValue("@TEXT1", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@TEXT2", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@TEXT3", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@TEXT4", textBox4.Text);
+
+                    con.Open();
+                    cmd.ExecuteScalar();
+                    con.Close();
+
+                    refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Убедитесь, что все поля заполнены.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if ((con.State != ConnectionState.Closed)||(con != null))
+                {
+                    con.Close();
+                }
+            }
+        }
+
+
+        public void refresh()
+        {
+            SqlConnection con = null;
+            SqlCommand cmd = null;
+            SqlDataReader red = null;
+
+            try
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                con = new SqlConnection(keycon);
+                cmd = new SqlCommand(req[0], con);
+                con.Open();
+                red = cmd.ExecuteReader();
+
+                dataGridView1.Columns.Add("ID", "ID");
+                dataGridView1.Columns.Add("ФИО", "ФИО");
+                dataGridView1.Columns.Add("СданныеПрофвзносы", "СданныеПрофвзносы");
+                dataGridView1.Columns.Add("Месяц", "Месяц");
+                dataGridView1.Columns.Add("Сумма", "Сумма");
+                while (red.Read())
+                {
+                    string[] temp = { red[0].ToString(), red[1].ToString(), red[2].ToString(), red[3].ToString(), red[4].ToString() };
+
+                    dataGridView1.Rows.Add(temp);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if ((con.State != ConnectionState.Closed) || (con != null))
+                {
+                    con.Close();
+                }
             }
         }
     }
